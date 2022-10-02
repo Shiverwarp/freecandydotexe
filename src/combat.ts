2148913544b6de09974314a373c7c8a7e150a700
Shiverwarp <Lorenz69@gmail.com>
2022-10-02 09:42:35 -0600

    extra bowl

--- src/combat.ts
+++ src/combat.ts
@@ -70,7 +70,7 @@ export default class Macro extends StrictMacro {
     return this.try([
       $skill`Curse of Weaksauce`,
       $skill`Micrometeorite`,
-      $skill`Bowl Straight Up`,
+      $skill`Shadow Noodles`,
       $skill`Shell Up`,
       $item`Time-Spinner`,
       $item`little red book`,
